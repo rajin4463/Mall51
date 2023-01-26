7035c0b921faa2b7e2cc039ab1df92891ae96d60
rajin4463 <73782473+rajin4463@users.noreply.github.com>
2023-01-26 19:47:41 +0530

    Add files via upload

--- shopDash.js
+++ shopDash.js
@@ -6,6 +6,7 @@ const tag = document.getElementById('tagAdd');
 const save = document.getElementById('save');
 
 let category;
+let state = true;
 
 //on load display info of shop on webpage 
 window.addEventListener("load", function(){
@@ -45,31 +46,21 @@ tag.addEventListener('click', (e)=>{
 
 //function to convert images to base64
 const toBase64 = file => new Promise((reslove, reject)=>{
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () => reslove(reader.result)
-    reader.onerror = err => reject(err);
+    try{
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = () => reslove(reader.result)
+        reader.onerror = err => reject(err);
+        // return true
+    }
+    catch(err){
+        // return false
+        console.log(err);
+    }
 })
 
-//main save function that saved data to the database
-save.addEventListener('click', async (e)=>{
-    e.preventDefault()
-    const fileInput = document.getElementById('image').files[0]
-    const discount = document.getElementById('discounts').checked;
-    const tags = category
-
-    let state = true
-
-    let result;
-    async function main(){
-        result = await toBase64(fileInput);
-    }
-    await main();
-
-    const ID = localStorage.getItem('ShopID')
-    let ShopID = Number(ID)
-    let ImgID = ShopID
-    let image = result
+//function to save image
+function addIMG(ShopID, ImgID, image){
     fetch(BASEURL+'shopdash/add-img', {
         method: "PATCH",
         body: JSON.stringify({ShopID:ShopID, ImgID:ImgID, img:image}),
@@ -81,9 +72,14 @@ save.addEventListener('click', async (e)=>{
     .then((json) => {
         if(!json.status == "Success"){
             state = false
+        }else{
+            state = true
         }
     });
+}
 
+//function to update tags
+function updateTAGS(tags, discount, ShopID){
     fetch(BASEURL+'shopdash/update/'+ShopID, {
         method: "PATCH",
         body: JSON.stringify({Category:tags, Discounts:discount}),
@@ -95,12 +91,51 @@ save.addEventListener('click', async (e)=>{
     .then((json) => {
         if(!json.status == "data saved"){
             state = false
-        }
-
-        if(state == true){
-            alert("Data saved")
         }else{
-            alert("Error! Data not saved")
+            state = true
         }
     })
+}
+
+//main save function that saved data to the database
+save.addEventListener('click', async (e)=>{
+    e.preventDefault()
+    const fileInput = document.getElementById('image').files[0]
+    const discount = document.getElementById('discounts').checked;
+
+    let imgCont;
+
+    let result;
+    if(!document.getElementById('image').value==''){
+        async function main(){
+            result = await toBase64(fileInput);
+        }
+        await main();
+        imgCont = true
+    }else{
+        imgCont = false
+    }
+    const ID = localStorage.getItem('ShopID')
+    let ShopID = Number(ID)
+    let ImgID = ShopID
+    let image = result
+
+    try{
+        if(imgCont == true){
+            img = addIMG(ShopID, ImgID, image)
+        }
+    }
+    catch(err){
+        console.log(err);
+        state = false
+    }
+
+    const unique = Array.from(new Set(category));
+    updateTAGS(unique, discount, ShopID);
+
+    if(state == true){
+        alert("Data saved!")
+    }else{
+        alert("Error! Data not saved")
+    }
 })
